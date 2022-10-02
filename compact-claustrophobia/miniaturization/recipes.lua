a6091a12410f794b8796403aae3a2a75b8c35ae3
DiffuseHyperion <xieyingjie08@gmail.com>
2022-10-02 20:23:17 +0800

    Update recipes.lua
    added tunnels, giant compact machine and maximum compact machine

--- compact-claustrophobia/miniaturization/recipes.lua
+++ compact-claustrophobia/miniaturization/recipes.lua
@@ -4,9 +4,13 @@ local RS = "minecraft:redstone"
 local GDB = "minecraft:gold_block"
 local INB = "minecraft:iron_block"
 local EP = "minecraft:ender_pearl"
+local HPR = "minecraft:hopper"
 local AIR = "minecraft:air"
 
 local CMW = "compactmachines3:wallbreakable"
+local CSE = "modularmachinery:blockcasing"
+local GLM = "contenttweaker:glitched3"
+local GGM = "contenttweaker:glitched4"
 
 recipes = {
     enderpearl = {
@@ -148,7 +152,123 @@ recipes = {
         activation = EP,
         delay = 25,
         label = "normal compact machine"
-    }
+    },
+    tunnel = {
+        structure = {
+            {
+                {RS, RS, RS},
+                {RS, CMW, RS},
+                {RS, RS, RS},
+            },
+            {
+                {AIR, AIR, AIR},
+                {AIR, HPR, AIR},
+                {AIR, AIR, AIR},
+            },
+            {
+                {AIR, AIR, AIR},
+                {AIR, AIR, AIR},
+                {AIR, AIR, AIR},
+            }
+        },
+        required = {
+            {
+                name = RS,
+                quantity = 9
+            },
+            {
+                name = CMW,
+                quantity = 1
+            },
+            {
+                name = HPR,
+                quantity = 1
+            }
+        },
+        activation = RS,
+        delay = 15,
+        label = "tunnel"
+    },
+    giantcompactmachine = {
+        structure = {
+            {
+                {CMW, CMW, CMW},
+                {CMW, GLM, CMW},
+                {CMW, CMW, CMW},
+            },
+            {
+                {CMW, GLM, CMW}, 
+                {GLM, CSE, GLM},
+                {CMW, GLM, CMW},
+            },
+            {
+                {CMW, CMW, CMW},
+                {CMW, GLM, CMW},
+                {CMW, CMW, CMW},
+            }
+        },
+        required = {
+            {
+                name = CMW,
+                quantity = 20
+            },
+            {
+                name = GLM,
+                quantity = 6
+            },
+            {
+                name = CSE,
+                quantity = 1
+            },
+            {
+                name = EP,
+                quantity = 1
+            }
+        },
+        activation = EP,
+        delay = 50,
+        label = "giant compact machine"
+    },
+    maximumcompactmachine = {
+        structure = {
+            {
+                {CMW, CMW, CMW},
+                {CMW, GGM, CMW},
+                {CMW, CMW, CMW},
+            },
+            {
+                {CMW, GGM, CMW}, 
+                {GGM, CSE, GGM},
+                {CMW, GGM, CMW},
+            },
+            {
+                {CMW, CMW, CMW},
+                {CMW, GGM, CMW},
+                {CMW, CMW, CMW},
+            }
+        },
+        required = {
+            {
+                name = CMW,
+                quantity = 20
+            },
+            {
+                name = GGM,
+                quantity = 6
+            },
+            {
+                name = CSE,
+                quantity = 1
+            },
+            {
+                name = EP,
+                quantity = 1
+            }
+        },
+        activation = EP,
+        delay = 50,
+        label = "maximum compact machine"
+    },
 }
 
 return recipes
